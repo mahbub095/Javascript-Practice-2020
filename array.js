6c6857af4c28f75fb5686971ef5d382d32038259
mahbub095 <mahbub.095@gmail.com>
2020-07-17 16:41:01 +0600

    Array

---
+++ array.js
@@ -0,0 +1,13 @@
+var friedsage = [14,16,18,20];
+//jodi variable rakte hoy
+var sonaliage = friedsage[2];
+//update korar jonno
+friedsage[1] =21;
+
+var position= friedsage.indexOf(20);
+console.log(friedsage)
+//console.log(friedsage[0])
+friedsage.push(55);
+//array te koyta element ache
+
+console.log(friedsage.length)
