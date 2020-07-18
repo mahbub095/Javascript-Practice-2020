0fd2f3d40c95c7c44c3860950ea136d04c1228bc
mahbub095 <mahbub.095@gmail.com>
2020-07-18 12:48:28 +0600

    Function parameter ,return

--- function.js
+++ function.js
@@ -1,4 +1,26 @@
-function sayloveyou(){
-    console.log('Jaan o baby');
+// function sayloveyou(){
+//     console.log('Jaan o baby');
+// }
+// sayloveyou();
+// doubleIt(5);
+// doubleIt(10);
+// doubleIt(100);
+
+
+function doubleIt(num){
+    var result = num*2;
+    //console.log(result);
+    return result;
 }
-sayloveyou();
+
+var first = doubleIt(5);
+var second = doubleIt(50);
+var total= first+second;
+console.log(total);
+
+function add (num1,num2){
+    var result =num1+num2;
+    return result;
+}
+var sum =add(15,15);
+console.log(sum);
