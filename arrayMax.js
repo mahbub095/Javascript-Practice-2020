98db1f241a14df6e4dee83ecc5a87dd5a5c09a48
mahbub095 <mahbub.095@gmail.com>
2020-07-20 23:16:55 +0600

    Factorial while loop

---
+++ arrayMax.js
@@ -0,0 +1,27 @@
+// var marks = [45,81,63,98,56,35,23];
+// //var max = marks[0];
+// var sum =0;
+// for(var i= 0;i<marks.length;i++){
+//     var element =marks[i];
+//     sum = sum+element;
+//     // if(element>max){
+//     //     max=element;
+//     // }
+// }
+// console.log(sum);
+
+
+var marks = [45,81,63,98,56,35,23];
+function getSumArray(marks){
+    var sum =0;
+for(var i= 0;i<marks.length;i++){
+    var element =marks[i];
+    sum = sum+element;
+    // if(element>max){
+    //     max=element;
+    // }
+}
+    return sum;
+ 
+}
+console.log(sum);
