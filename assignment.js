639028e1a22b3f6fffaecf068306b61c9ceff433
mahbub095 <mahbub.095@gmail.com>
2020-07-22 01:01:05 +0600

    brickcalculator

--- assignment.js
+++ assignment.js
@@ -14,5 +14,25 @@ function woodCalculator(chair, table, bed) {
     totalWood = (chair * 1) + (table * 3) + (bed * 5);
     console.log("Total wood needed : " + totalWood);
 }
-//! Output check
+
 woodCalculator(5, 8, 10);
+
+//3.  brickCalculator
+
+function brickCalculator(floor){
+    let result;
+    if(floor<=10){
+        result=floor*15*1000;
+    }else if(floor>=11 && floor<=20){
+        result =10*15*1000+(floor-10)*12*1000;
+    }
+    else if( floor>=21){
+        result =10*15*1000 + 10*12*1000 + (floor-20)*10*1000;
+    }
+   
+    return result;
+    
+}
+ console.log(brickCalculator(25));
+
+ 
