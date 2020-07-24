b847f49c339a348417dbe580ea5aea88ac8d6bbc
mahbub095 <mahbub.095@gmail.com>
2020-07-24 12:48:13 +0600

    array max & practice

--- arrayMax.js
+++ arrayMax.js
@@ -11,17 +11,27 @@
 // console.log(sum);
 
 
-var marks = [45,81,63,98,56,35,23];
-function getSumArray(marks){
-    var sum =0;
-for(var i= 0;i<marks.length;i++){
-    var element =marks[i];
-    sum = sum+element;
-    // if(element>max){
-    //     max=element;
-    // }
-}
-    return sum;
+// var marks = [45,81,63,98,56,35,23];
+// function getSumArray(marks){
+//     var sum =0;
+// for(var i= 0;i<marks.length;i++){
+//     var element =marks[i];
+//     sum = sum+element;
+//     // if(element>max){
+//     //     max=element;
+//     // }
+// }
+//     return sum;
  
+// }
+// console.log(sum);
+
+var mark = [45,56,77,88,99,111];
+var max = mark[0];
+for(var i=0; i<mark.length;i++){
+    var element =mark[i];
+    if(element>max){
+        max=element;
+    }
 }
-console.log(sum);
+console.log("Highest Value : ", max);
---
+++ practice-a.js
@@ -0,0 +1,9 @@
+function largest(numbers){
+    //var larger =numbers[0];
+    for(var i =0;i<numbers.length;i++){
+        var element =numbers[i];
+        console.log(element);
+    }
+
+}
+var output = largest([3,4,5]);
