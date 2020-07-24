7a881daf1ff850e444104f2f0d0a1826d637930e
mahbub095 <mahbub.095@gmail.com>
2020-07-24 14:20:59 +0600

    Dom+recursion practice

---
+++ dom-events/script.js
@@ -0,0 +1,2 @@
+console.log(81);
+alert('I am learning Javascript');
---
+++ recursion.js
@@ -0,0 +1,9 @@
+function factorial (num){
+    var fact =1;
+    for(var i =1; i<=num;i++){
+        fact =fact*i;
+        console.log(i,fact);
+    }
+}
+var result =factorial(5);
+console.log('result',result);
