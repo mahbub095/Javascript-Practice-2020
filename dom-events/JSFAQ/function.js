a0c8391860a77085cc7e26e71f7a296bb7a4f138
mahbub095 <mahbub.095@gmail.com>
2020-07-31 01:01:52 +0600

    Function FAQ

---
+++ dom-events/JSFAQ/function.js
@@ -0,0 +1,46 @@
+// // nums = [5,12,89,45,18,8];
+// // for(let i = 0;i<nums.length;i++){
+// //     const num = nums[i];
+// //    // console.log(num*2);
+// //    if(num % 2 ==0){
+// //        console.log(num, ':is event number');
+// //    }
+// //    else{
+// //        console.log(num*2,':is odd number');
+// //    }
+// // }
+
+// function evenify(num){
+//     var result;
+//     if(num % 2 ==0){
+//         //console.log(num, ':is event number');
+//         result = num;
+//     }
+//     else{
+//         //console.log(num*2,':is odd number');
+//         result = num*2;
+//     }
+//     return result;
+// }
+ 
+// var result = evenify(3);
+// console.log('result' ,result);
+// nums = [5,12,89,45,18,8];
+
+// function evenify_all(nums){
+// for(let i= 0;i<nums.length;i++){
+//     const num = nums[i];
+//     evenify_all(num);
+// }
+// }
+
+// evenify_all(55);
+// console.log(evenify_all);
+
+function doubleNumber( num) {
+
+ 
+    result = num * 2;
+   }
+   var result = doubleNumber(31);
+   console.log(result);
